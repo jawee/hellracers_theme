836a7b15de4ed33eef53fde85cb86c5fbb719c77
Andreas Olsson <olsson.andreas@gmail.com>
2018-05-13 08:16:34 +0000

    Driver page

---
+++ page-templates/driver-list-page.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: Driver list page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+$container = get_theme_mod( 'understrap_container_type' );
+get_header();
+?>
+
+<div class="wrapper" id="driver-wrapper">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row news-frontpage">
+				<?php
+				$driverpages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
+
+				print_r($driverpages);
+
+				?>
+				<?php
+				  $count = 0;
+				  foreach($driverpages as $driverpage) {
+				    ?>
+						<div class="col-md">
+							<div class="news-content">
+								<div class="image-container" style="background-image: url(<?php echo get_the_post_thumbnail_url($driverpage->ID); ?>)">
+								</div>
+					    	<!-- <img src="<?php echo get_the_post_thumbnail_url($driverpage->ID); ?>" class="img-fluid"> -->
+					      <!-- <div class="carousel-caption d-none d-md-block"> -->
+				        
+				        <a href="<?php echo get_permalink($driverpage->ID); ?>" class=""><h3><?php echo $driverpage->post_title; ?></h3></a>
+							</div>
+				    </div>
+				    <?php
+				    $count++;
+						if($count%3 == 0) {
+							echo '</div>';
+							echo '<div class="row news-frontpage">';
+						}
+				  }
+
+					$remaining = $count%3;
+					for($i = 1; $i < $remaining; $i++) {
+						echo '<div class="col-sm"></div>';
+					}
+
+				?>
+	</div><!-- .row -->
+
+		<?php
+			
+
+			foreach( $mypages as $page ) {		
+				$content = $page->post_name;
+				if ( ! $content ) // Check for empty page
+					continue;
+
+				$content = apply_filters( 'the_content', $content );
+			?>
+				<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
+				<div class="entry"><?php echo $content; ?></div>
+			<?php
+			}	
+		?>
+
+	</div><!-- Container end -->
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
