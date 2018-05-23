177bf1a81f86998f8b40a08a64b711f04eb3515e
Andreas Olsson <olsson.andreas@gmail.com>
2018-05-23 16:55:23 +0000

    Footer

---
+++ footer.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+$the_theme = wp_get_theme();
+// $container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_sidebar( 'footerfull' ); ?>
+<div class="wrapper" id="wrapper-footer-top">
+	<div class="container">
+		<div class="row top-footer">
+			<div class="col-md-8">
+				<ul class="social-media-menu">
+					<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+					<li><a href="#"><i class="fa fa-youtube-square fa-2x"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<ul class="calendar-menu">
+					<li><a href="#"><i class="fa fa-calendar"></i>Ladda ner 2018 kalendern</a></li>
+				</ul>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="wrapper" id="wrapper-footer-bottom">
+
+	<div class="container">
+		
+
+		<div class="row">
+
+			<div class="col-md-12">
+				<footer class="site-footer" id="colophon">
+						<?php wp_nav_menu(
+							array(
+								'theme_location'  => 'footer',
+								'container_class' => 'footer-nav',
+								'container_id'    => 'footer-nav-container',
+								'menu_class'      => 'footer-nav',
+								'fallback_cb'     => '',
+								'menu_id'         => 'footer-menu',
+								'depth'           => 2,
+							)
+						); ?>
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+		<div class="row top-divider">
+			<div class="col-md-9">
+				<?php the_custom_logo(); ?>
+			</div>
+			<div class="col-md-3">
+				<p>&copy; <?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> <?php echo date("Y"); ?></p>
+			</div>
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
--- functions.php
+++ functions.php
@@ -30,3 +30,8 @@ function add_child_theme_textdomain() {
     load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+
+register_nav_menu(
+  'footer', __('Footer Navigation', 'understrap-child')  
+);
