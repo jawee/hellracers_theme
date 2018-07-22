a94680926866c2c262f6265d20af3f7cf80c3c0e
Andreas Olsson <olsson.andreas@gmail.com>
2018-07-22 11:40:36 +0000

    Front page styling and heading

--- front-page.php
+++ front-page.php
@@ -20,28 +20,28 @@ $container   = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="start-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
+		<h1 class="front-page-heading"><?php the_field('titel_field'); ?></h1>
 		<div class="row news-frontpage">
 				<?php
 				$args = array( 'numberposts' => 6, 'offset' => 3 );
 				$recent_posts = wp_get_recent_posts($args);
 				wp_reset_query();
-
 				?>
 				<?php
 				  $count = 0;
 				  foreach($recent_posts as $recent) {
 						$thumbnail_url = get_the_post_thumbnail_url($recent["ID"]);
+
+						$the_date = $post_date = get_the_date( 'j F Y', $recent);
 				    ?>
-						<div class="col-md-4">
-							<div class="news-content">
-								<div class="image-container" style="background-image: url(<?php echo $thumbnail_url; ?>)">
-								</div>
-					    	<!-- <img src="<?php echo $thumbnail_url; ?>" class="img-fluid"> -->
-					      <!-- <div class="carousel-caption d-none d-md-block"> -->
-				        <h3><?php echo $recent["post_title"]; ?></h3>
-				        <a href="<?php echo get_permalink($recent["ID"]); ?>" class="btn btn-primary">Läs mer</a>
+					<div class="col-md-4">
+						<div class="news-content">
+							<div class="image-container" style="background-image: url(<?php echo $thumbnail_url; ?>)"></div>
+							<div class="inner-content">
+								<span class="post-date"><i class="fa fa-clock-o" aria-hidden="true"></i><?php echo $the_date; ?></span>
+				        		<a href="<?php echo get_permalink($recent["ID"]); ?>"><h5><?php echo $recent["post_title"]; ?></h5></a>	
 							</div>
+						</div>
 				    </div>
 				    <?php
 				    
